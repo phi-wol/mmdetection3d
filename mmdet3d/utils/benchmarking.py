e825efba886dd5e5ca1a999239d9270c37964af4
phi-wol <philipp.wolters@tum.de>
2022-04-19 08:03:38 +0000

    added benchmarking class

---
+++ mmdet3d/utils/benchmarking.py
@@ -0,0 +1,20 @@
+import time
+
+def now():
+    return time.time() * 1000
+
+
+class Benchmark:
+    def __init__(self, step_name):
+        self.start = None
+        self.end = None
+        self.step_name = step_name
+
+    def __enter__(self): 
+        print(now(), f"started {self.step_name}")
+        self.start = now()
+        
+    def __exit__(self, ext, exv, trb):
+        self.end = now()
+        duration = self.end - self.time
+        print(now(), f"{self.step_name} took {duration} ms")
